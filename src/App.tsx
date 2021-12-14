7c3c006d5dcf02e002122cc6d8c559824d3fd7e0
Marcos Andre <marcosdev.me@gmail.com>
2021-12-14 14:32:23 -0300

    adding functions to move character

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import * as C from './App.styles';
 import { Character } from './components/Character';
 import { useCharacter } from './hooks/useCharacter';
@@ -5,9 +6,38 @@ import { useCharacter } from './hooks/useCharacter';
 const App = () => {
   const character = useCharacter();
 
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    switch(e.code) {
+      case 'KeyA':
+      case 'ArrowLeft':
+        character.moveLeft();
+      break;
+
+      case 'KeyW':
+      case 'ArrowUp':
+        character.moveUp();
+      break;
+
+      case 'KeyD':
+      case 'ArrowRight':
+        character.moveRight();
+      break;
+
+      case 'KeyS':
+      case 'ArrowDown':
+        character.moveDown();
+      break;
+    }
+  }
+
   return(
     <C.Container>
       <C.Map>
+        <Character x={5} y={2} />
         <Character x={character.x} y={character.y} />
       </C.Map>
     </C.Container>
