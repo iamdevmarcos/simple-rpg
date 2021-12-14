d3ffebe9b0e2c45ebe72f5d4eac0670a6d3ea7a8
Marcos Andre <marcosdev.me@gmail.com>
2021-12-14 15:43:14 -0300

    adding canMove

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { CharacterSides } from '../types/CharacterSides';
+import { mapSpots } from '../data/mapSpots';
 
 export const useCharacter = () => {
     const [pos, setPos] = useState({ x:3, y:5 });
@@ -7,34 +8,38 @@ export const useCharacter = () => {
 
     const moveLeft = () => {
         setPos(pos => ({
-            x: pos.x - 1,
+            x: canMove(pos.x - 1, pos.y) ? pos.x - 1 : pos.x,
             y: pos.y
         }));
-        setSide("left");
+        setSide('left');
     }
-
     const moveRight = () => {
         setPos(pos => ({
-            x: pos.x + 1,
+            x: canMove(pos.x + 1, pos.y) ? pos.x + 1 : pos.x,
             y: pos.y
         }));
-        setSide("right");
+        setSide('right');
     }
-
     const moveDown = () => {
         setPos(pos => ({
             x: pos.x,
-            y: pos.y + 1
+            y: canMove(pos.x, pos.y + 1) ? pos.y + 1 : pos.y
         }));
-        setSide("down");
+        setSide('down');
     }
-
     const moveUp = () => {
         setPos(pos => ({
             x: pos.x,
-            y: pos.y - 1
+            y: canMove(pos.x, pos.y - 1) ? pos.y - 1 : pos.y
         }));
-        setSide("up");
+        setSide('up');
+    }
+
+    function canMove(x: number, y: number) {
+        if(mapSpots[y] !== undefined && mapSpots[y][x] !== undefined) {
+            return (mapSpots[y][x] === 1);
+        }
+        return false;
     }
 
     return {
