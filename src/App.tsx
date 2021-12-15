40a71712ef726d02b11294ce37e1f3b5991c5cf1
Marcos Andre <marcosdev.me@gmail.com>
2021-12-15 10:31:46 -0300

    creating other player

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,36 @@ const App = () => {
   const character = useCharacter("Marcos");
   const [playerName, setPlayerName] = useState('');
   const [start, setStart] = useState(false);
-
+  const character2 = useCharacter(playerName);
+  
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
   }, []);
 
   const definePlayerName = (playerName: string) => {
     setPlayerName(playerName);
-    console.log(playerName);
   }
 
   const handleKeyDown = (e: KeyboardEvent) => {
     switch(e.code) {
         case 'KeyA':
         case 'ArrowLeft':
-        character.moveLeft();
+          character2.moveLeft();
         break;
 
         case 'KeyW':
         case 'ArrowUp':
-        character.moveUp();
+          character2.moveUp();
         break;
 
         case 'KeyD':
         case 'ArrowRight':
-        character.moveRight();
+          character2.moveRight();
         break;
 
         case 'KeyS':
         case 'ArrowDown':
-        character.moveDown();
+          character2.moveDown();
         break;
 
         case 'Enter':
@@ -68,7 +68,8 @@ const App = () => {
           {playerName && playerName !== '' &&
             <>
               <C.Map>
-                <Character x={character.x} y={character.y} side={character.side} name={character.name} />
+                <Character x={3} y={5} side={character.side} name={character.name} />
+                <Character x={character2.x} y={character2.y} side={character2.side} name={character2.name} />
               </C.Map>
             </>
           }
